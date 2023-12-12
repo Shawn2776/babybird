0313ba7ce5e31e4a610e687a737eb2d0be64c1b0
sdharrin <138178989+sdharrin@users.noreply.github.com>
2023-12-11 20:50:02 -0800

    added g analytics

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import AuthProvider from "../utils/providers/auth/AuthProvider";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import QueryProvider from "@/utils/providers/query/QueryProvider";
+import Script from "next/script";
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -17,6 +18,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-TN1GYE9DZ3`}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-TN1GYE9DZ3');
+        `}
+      </Script>
       <AuthProvider>
         <body
           className={`${roboto.className} bg-gradient-to-b from-oxford via-cambridge to-bittersweet min-h-screen`}
