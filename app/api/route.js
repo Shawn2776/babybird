2f6a6d06708751915430bbedd03e01b4d99117f0
sdharrin <138178989+sdharrin@users.noreply.github.com>
2023-12-14 16:45:51 -0800

    removed api route.js

--- app/api/route.js
+++
@@ -1,9 +0,0 @@
-import { getServerSession } from "next-auth";
-import { options } from "./auth/[...nextauth]/options";
-import { NextResponse } from "next/server";
-
-export async function GET(request) {
-  const session = await getServerSession(options);
-  console.log("GET API", session);
-  return NextResponse.json({ authenticated: !!session });
-}
