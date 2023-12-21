e5beb7470ed8621dcd4fa0fcbb6e8cc08ffdf7d1
sdharrin <138178989+sdharrin@users.noreply.github.com>
2023-12-21 15:22:24 -0800

    moved account page around

--- app/UserAccount/[username]/page.jsx
+++ app/UserAccount/[username]/page.jsx
@@ -1,19 +1,32 @@
 "use client";
 
-import ProfileCard from "@/components/talkComponents/ProfileCard";
-import AccountCard from "@/components/talkComponents/accountTalks/AccountCard";
-import Talk from "@/components/talkComponents/accountTalks/TalkCard";
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
 
 const url = process.env.MAIN_URL;
 
-export default function Page({ params }) {
-  return (
-    <>
-      <AccountCard />
-      {/* <Talk /> */}
-    </>
-  );
-}
+export const Page = ({ params }) => {
+  const userQuery = useQuery({
+    queryKey: ["user"],
+    queryFn: async () => {
+      const response = await fetch("/api/userAccount/");
+
+      if (!response.ok) {
+        router.replace("/Login");
+      }
+
+      const data = await response.json();
+      return data;
+    },
+  });
+
+  if (userQuery.isLoading) return <div>Loading...</div>;
+
+  if (userQuery.isError) return <div>Error fetching user profile</div>;
+
+  if (userQuery.isSuccess) {
+    const user = userQuery.data;
+
+    return <div>{user.name}</div>;
+  }
+};
 
